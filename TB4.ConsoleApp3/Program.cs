53cd9fb2fcf02020da1545edfd8cae24bee6ade4
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-12-08 21:41:14 +0630

    feat: ADO.NET CRUD

---
+++ TB4.ConsoleApp3/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Runtime.InteropServices;
+
+// S - C - S - B
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "."; // server name
+sqlConnectionStringBuilder.InitialCatalog = "Batch4MiniPOS"; // database name
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sasa@123";
+sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+string connectionString = sqlConnectionStringBuilder.ConnectionString;
+Console.WriteLine(connectionString);
+
+SqlConnection connection = new SqlConnection(connectionString);
+
+connection.Open();
+
+#region Read
+
+//SqlCommand cmd = new SqlCommand("select * from tbl_product", connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt); // execute
+
+#endregion
+
+#region Create
+
+string query = @"INSERT INTO [dbo].[Tbl_Product]
+           ([ProductName]
+           ,[Price]
+           ,[Quantitty]
+           ,[IsDelete]
+           ,[CreatedDateTime])
+     VALUES
+           ('Pineapple'
+           ,4000
+           ,30
+           ,0
+           ,GETDATE())";
+SqlCommand cmd = new SqlCommand(query, connection);
+int result = cmd.ExecuteNonQuery();
+
+#endregion
+
+connection.Close();
+
+string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+Console.WriteLine(message);
+
+//// table > row > column
+//foreach (DataRow dr in dt.Rows)
+//{
+//    string productName = Convert.ToString(dr["ProductName"]);
+//    decimal price = Convert.ToDecimal(dr["Price"]);
+//    Console.WriteLine("Product Name : " + productName);
+//    Console.WriteLine($"Price : {price.ToString("n0")}");
+//}
+
+//Console.WriteLine("Connection Opening...");
+//connection.Open();
+//Console.WriteLine("Connection Opened.");
+
+//Console.WriteLine("Connection Closing...");
+//connection.Close();
+//Console.WriteLine("Connection Closed.");
+
+Console.ReadLine();
